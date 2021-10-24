d47d1d076f1a3c56c793d5dd60496691de47d131
javifarina <javifarina@gmail.com>
2021-10-24 02:51:57 -0300

    CRUD lista de Tareas

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import ListaTareas from "./Componets/ListaTareas";
+function App() {
+  return (
+    <div className="container mt-5">
+      <ListaTareas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componets/ListaTareas.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+
+const ListaTareas = () => {
+  //Estados
+  const [tarea, setTarea] = useState("");
+  const [gkey, setGkey] = useState(0);
+  const [tareas, setTareas] = useState([]);
+  const [modoEdicion, setModoEdicion] = useState(false);
+  const [id, setId] = useState("");
+  const [error, setError] = useState(null);
+  //genera ID
+  const generateKey = () => {
+    setGkey(gkey + 1);
+  };
+
+  //Funcion Agregar Tarea
+  const agregarTarea = (e) => {
+    e.preventDefault();
+    console.log(tarea);
+    //Validar Tareas
+    if (!tarea.trim()) {
+      console.log("elemento Vacio");
+      setError("Por Favor Ingrese una Tarea");
+      return;
+    }
+    generateKey();
+    setTarea("");
+    setTareas([...tareas, { id: gkey, nombreTarea: tarea }]);
+    setError(null);
+  };
+  //Eliminar Tareas
+  const eliminarTarea = (id) => {
+    console.log(id);
+    const filtrado = tareas.filter((e) => e.id !== id);
+    setTareas(filtrado);
+  };
+  //Boton Editar Tareas
+  const editar = (item) => {
+    console.log(item);
+    setModoEdicion(true);
+    setTarea(item.nombreTarea);
+    setId(item.id);
+  };
+
+  //Editar Tarea
+
+  const editarTarea = (e) => {
+    e.preventDefault();
+    if (!tarea.trim()) {
+      console.log("Elemento vacio");
+      setError("Por Favor Ingrese una Tarea");
+      return;
+    }
+    const arrayEditado = tareas.map((item) =>
+      item.id === id ? { id, nombreTarea: tarea } : item
+    );
+    setTareas(arrayEditado);
+    setModoEdicion(false);
+    setTarea("");
+    setId("");
+    setError(null);
+  };
+
+  return (
+    <>
+      <h1 className="text-center">Crud- Lista de Tareas</h1>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+            {tareas.length === 0 ? (
+              <li className="list-group-item">No Hay Elementos en la Lista</li>
+            ) : (
+              tareas.map((item) => (
+                <li className="list-group-item" key={item.id}>
+                  <span className="lead">{item.nombreTarea}</span>
+                  <button
+                    className="btn btn-danger btn-sm float-lg-end mx-2"
+                    onClick={() => eliminarTarea(item.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <button
+                    className="btn btn-warning btn-sm float-lg-end"
+                    onClick={() => editar(item)}
+                  >
+                    Editar
+                  </button>
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">
+            {modoEdicion ? "Editar Tarea" : "Agregar Tarea"}
+          </h4>
+          <form onSubmit={modoEdicion ? editarTarea : agregarTarea}>
+            {error ? <span className="text-danger">{error}</span> : null}
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Ingrese Tarea"
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <div class="d-grid gap-2">
+              {modoEdicion ? (
+                <button className="btn btn-success btn-block" type="submit">
+                  Guardar Cambios
+                </button>
+              ) : (
+                <button className="btn btn-dark btn-block" type="submit">
+                  Agregar
+                </button>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ListaTareas;
